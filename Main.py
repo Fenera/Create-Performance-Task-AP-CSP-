05b1a5dcad1b376932ccdc2689e074337df06e53
Fenera Taye <Fenerataye@gmail.com>
2023-03-24 09:34:46 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+pygame.font.init()
+
+length = 600
+width = 800
+
+#Creates window
+window = pygame.display.set_mode((length, width))
+
+#colors
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+run = True
+
+while run:
+    
+
