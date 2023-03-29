3fe2fce3ff3b19a9a20ad337b61c283e355fa414
Fenera Taye <Fenerataye@gmail.com>
2023-03-29 10:16:39 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,21 +1,75 @@
 import pygame
+import sys
+import random
+import pygame_gui
+
+
 
 pygame.init()
 pygame.font.init()
 
-length = 600
-width = 800
-
-#Creates window
-window = pygame.display.set_mode((length, width))
+#Dimensions of the window
+length = 1000
+height = 600
 
 #colors
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+sand = (255,205,116)
+borange = (255,114,81)
+modern3 = (14, 52, 91)
+colors = [red, green, blue, white, black, sand, borange, modern3]
 
+#Creates window
+window = pygame.display.set_mode((length, height))
+#Caption
+pygame.display.set_caption("Testing")
+#Initialize Font
+font1 = pygame.font.SysFont("arialrounded", 100)
+
+manager = pygame_gui.UIManager((length, height))
+
+clock = pygame.time.Clock()
+FPS = 60
+
+text_input = pygame_gui.elements.UITextEntryLine(relative_rect = pygame.Rect((100, 200), (300, 50)), manager = manager, object_id = "#main_text_entry")
 run = True
 
+def color_change():
+    random_value = random.randint(0, 7)
+    return colors[random_value]
+
+
 while run:
+    clock.tick(FPS)/1000
+    #background color
+    color_back = color_change()
+    window.fill(color_back)
+    #Refresh rate for UI
+    #UI_RR = clock.tick(60)/1000
+
+
+    for event in pygame.event.get():
+        #Program ends when 'x' pressed
+        if event.type == pygame.QUIT:
+            run = False
+
+        manager.process_events(event)
+
+       #manager.update(UI_RR)
+
+        manager.draw_ui(window)
+
+
+
+
+    font_test = font1.render("Q.2.1", True, modern3)
+    window.blit(font_test, (400, 50))
+    clock.tick(60)
+    pygame.display.update()
     
 
+pygame.quit()
