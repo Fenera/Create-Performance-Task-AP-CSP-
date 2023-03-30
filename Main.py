bf229aaf21569d657da51f35c479d6f3e3b0ea7e
Fenera Taye <Fenerataye@gmail.com>
2023-03-30 09:52:17 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import pygame_gui
 
 
 pygame.init()
-pygame.font.init()
+#pygame.font.init()
 
 #Dimensions of the window
 length = 1000
@@ -28,26 +28,46 @@ window = pygame.display.set_mode((length, height))
 #Caption
 pygame.display.set_caption("Testing")
 #Initialize Font
-font1 = pygame.font.SysFont("arialrounded", 100)
+font1 = pygame.font.SysFont("arialrounded", 50)
 
 manager = pygame_gui.UIManager((length, height))
 
 clock = pygame.time.Clock()
 FPS = 60
 
-text_input = pygame_gui.elements.UITextEntryLine(relative_rect = pygame.Rect((100, 200), (300, 50)), manager = manager, object_id = "#main_text_entry")
+text_input = pygame_gui.elements.UITextEntryLine(relative_rect = pygame.Rect((150, 200), (700, 50)), manager = manager, object_id = "#main_text_entry")
 run = True
 
-def color_change():
-    random_value = random.randint(0, 7)
-    return colors[random_value]
+
+#List to store questions
+
+
+
+
+
+def display_text(text_display):
+    while True:
+    #background color
+        window.fill(sand)
+        new_text = pygame.font.SysFont("arial", 100).render(f"Answer: {text_display}", True, "black")
+        new_text_rect = new_text.get_rect(center = (length/2, height/2))
+
+        window.blit(new_text, new_text_rect)
+        clock.tick(60)
+        for event in pygame.event.get():
+            #Program ends when 'x' pressed
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        pygame.display.update()
+
 
 
 while run:
-    clock.tick(FPS)/1000
+
+    refreshrate_ui = clock.tick(60)/750 #The /750 controls how fast the cursor blinks
     #background color
-    color_back = color_change()
-    window.fill(color_back)
+    window.fill(sand)
     #Refresh rate for UI
     #UI_RR = clock.tick(60)/1000
 
@@ -56,20 +76,24 @@ while run:
         #Program ends when 'x' pressed
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
+            display_text(event.text)
 
         manager.process_events(event)
 
-       #manager.update(UI_RR)
+    manager.update(refreshrate_ui)
 
-        manager.draw_ui(window)
-
-
+    manager.draw_ui(window)
 
 
-    font_test = font1.render("Q.2.1", True, modern3)
-    window.blit(font_test, (400, 50))
+
+
+    test_text = font1.render("Which continent is the largest?", True, modern3)
+    window.blit(test_text, (150, 50))
     clock.tick(60)
     pygame.display.update()
     
 
 pygame.quit()
+
+
