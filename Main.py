0a9955d4fca2708d62fd9a64246ba269d4da75c0
Fenera Taye <Fenerataye@gmail.com>
2023-04-11 09:56:21 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,11 +2,20 @@ import pygame
 import sys
 import random
 import pygame_gui
+from Question import quest_ans
 
 
 
 pygame.init()
 #pygame.font.init()
+
+#1. Create a seperate function that takes a question in 
+#2. Use that function to display the question using GUI
+#3. 
+
+
+
+
 
 #Dimensions of the window
 length = 1000
@@ -45,15 +54,9 @@ question_bank = ["Who was the first woman to fly solo across the Atlantic Ocean?
 answer_bank = ["amelia Earhart", "amphibians", "skin", "gross domestic product"]
 
 
-
-def generate_question():
-    index = random.randint(0, len(question_bank) - 1)
-
-    return question_bank[index]
-
-
-
-
+def check_correct(question, response):
+    if(quest_ans[quest_ans] == response.capitalize()):
+        return True
 
 
 
@@ -63,7 +66,7 @@ def display_text(text_display):
     while True:
     #background color
         window.fill(sand)
-        new_text = pygame.font.SysFont("arial", 100).render(f"Answer: {text_display}", True, "black")
+        new_text = pygame.font.SysFont("arial", 100).render(f"Your Answer: {text_display}", True, "black")
         new_text_rect = new_text.get_rect(center = (length/2, height/2))
 
         window.blit(new_text, new_text_rect)
@@ -85,13 +88,20 @@ while run:
     #Refresh rate for UI
     #UI_RR = clock.tick(60)/1000
 
+    test_text = font1.render("What is the capital of Kenya?", True, modern3)
+    window.blit(test_text, (150, 50))
+    clock.tick(60)
 
     for event in pygame.event.get():
         #Program ends when 'x' pressed
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
-            display_text(event.text)
+            if(check_correct("What is the capital of Kenya?", event.text)):
+                display_text("Correct!! +1 point")
+            else:
+                display_text("Incorrect!! The correct answer is 'Nairobi'")
+            #event.text is what the user wrote and entered
 
         manager.process_events(event)
 
@@ -102,14 +112,10 @@ while run:
 
 
     
-    test_text = font1.render(generate_question(), True, modern3)
-    window.blit(test_text, (150, 50))
-    clock.tick(60)
+    
 
         
     pygame.display.update()
     
 
 pygame.quit()
-
-
