de8d9fb0e09a9a9046660a265b535f44f9d31acb
Fenera Taye <Fenerataye@gmail.com>
2023-04-11 09:56:45 -0500

    Update Question.py

---
+++ Question.py
@@ -0,0 +1,9 @@
+
+quest_ans = {"Who was the first woman to fly solo across the Atlantic Ocean?": "Amelia Earhart",
+            "What is the capital of Italy?": "Rome",
+             "What does WWW stand for in a website browser": "World wide web",
+             "What is the capital of Kenya?": "Nairobi"}
+
+
+
+
