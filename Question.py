b158d925fd57124097ed1bbd3ed92edfd381dca4
Fenera Taye <Fenerataye@gmail.com>
2023-04-18 10:06:10 -0500

    Update Question.py

--- Question.py
+++ Question.py
@@ -1,10 +1,15 @@
-quest_ans = {"Who was the first human to step on the Moon?": "Neil Armstrong",
-            "What is the capital of Italy?": "Rome",
-             "What does WWW stand for in a website browser": "World wide web",
-             "What is the capital of Kenya?": "Nairobi",
-             "How many elements are in the periodic table?": "118",
-             "What is the longest river in the world?":"Nile",
-             "What is the hardest natural substance in the world?": "Diamond",
-             "What is the longest bone in the human body?": "Femur",
-             "Which planet is closest to Earth?":"Mercury",
-             "Which continent has the most countries?": "Africa"}
+quest_ans = {"What does a flashing red light indicate?": "a",
+             "What should you do if you encounter a flooded roadway?": "c",
+             "What is the speed limit in business and residential areas?": "b"}
+
+#"What is the capital of Italy?": "Rome",
+            # "What does WWW stand for in a website browser": "World wide web",
+            # "What is the capital of Kenya?": "Nairobi",
+            # "How many elements are in the periodic table?": "118",
+            # "What is the longest river in the world?":"Nile",
+            # "What is the hardest natural substance in the world?": "Diamond",
+            # "What is the longest bone in the human body?": "Femur",
+            # "Which planet is closest to Earth?":"Mercury",
+           #  "Which continent has the most countries?": "Africa"}
+
+#Sources: https://quizlet.com/5882724/permit-practice-test-flash-cards/, https://quizlet.com/42356918/permit-test-answers-flash-cards/, https://quizlet.com/202487360/dmv-learners-permit-test-flash-cards/
