d1b83cbbed201b098501656831de4f1b977fa25f
Fenera Taye <Fenerataye@gmail.com>
2023-04-18 19:09:15 -0500

    Update Question.py

--- Question.py
+++ Question.py
@@ -1,6 +1,18 @@
 quest_ans = {"What does a flashing red light indicate?": "a",
              "What should you do if you encounter a flooded roadway?": "c",
-             "What is the speed limit in business and residential areas?": "b"}
+             "What is the speed limit in business and residential areas?": "b",
+             "Signs with orange backgrounds mean:": "b",
+             "If the rear end of your car starts skidding to the left, you should:" : "c",
+             "When driving at night, you should: ": "a",
+             "When driving in fog, rain, or snow, use:" : "b",
+             "You should use your horn when: ": "b",
+             "At intersections, crosswalks, and railroad crossings, you should always: " : "c",
+             "Alcohol in any concentration is: " : "a",
+             "If a road is slippery, maintain a following distance that is: " : "a",
+             "You see pedestrians near the road. You should: " : "c",
+             "If the roadway is wet or icy, you should: " : "b",
+             "At an intersection with stop signs on all corners, yield the right-of-way to any driver: ": "b",
+             "Roadways are the most slippery: " : "a"}
 
 #"What is the capital of Italy?": "Rome",
             # "What does WWW stand for in a website browser": "World wide web",
