3c0eb05948121494f6a14bab8ac4fd8e3d0d5c15
Fenera Taye <Fenerataye@gmail.com>
2023-04-21 09:54:38 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
+#Asset sources
+#check mark downloaded from https://pngtree.com/freepng/green-check-mark-icon-design-template-vector_6219078.html 
+#'x' mark downloaded from https://pngtree.com/freepng/red-x-fork_6001925.html
+
+#import all the needed modules/libraries
 import pygame
 import sys
 import pygame_gui
@@ -63,6 +68,7 @@ font_largest = pygame.font.SysFont("arialrounded", 65)
 font1 = pygame.font.SysFont("arialrounded", 50)
 font3 = pygame.font.SysFont("arialrounded", 30)
 font2 = pygame.font.SysFont("arialrounded,", 20)
+font4 = pygame.font.SysFont("arialrounded", 40)
 
 #Creates a ui manager to be used in text box
 manager = pygame_gui.UIManager((length, height))
@@ -81,14 +87,14 @@ list_of_answers = ["(a)Come to a complete stop | (b)Yield to oncoming traffic | 
                    "(a)Steer right | (b)Keep steering wheel straight | (c) Steer left",
                    "(a)Increase following distance | (b)Use highbeams | (c) Decrease following distance",
                    "(a)High beams | (b)Low beams | (c)Only fog lights",
-                   "(a) Another vehicle is in your way | (b) It may help prevent a collision | (c) Animal on road",
-                   "(a) Stop and proceed cautiously | (b) Slowly pass vehicles that seem to be stopped | (c) Look to the sides of your vehicle to see what is coming",
+                   "(a)Another vehicle is in your way | (b) It may help prevent a collision | (c) Animal on road",
+                   "(a)Stop and proceed cautiously | (b) Slowly pass vehicles that seem to be stopped | (c) Look to the sides of your vehicle to see what is coming",
                    "(a)A depressant | (b)A stimulant | (c)Both",
                    "(a)Farther from the car ahead than normal | (b)No different than normal | (c)Closer to the car ahead than normal",
-                   "(a)Sound your horn and maintain your speed | (b)Speed up to clear the area quickly | (c)Slow down and pass with caution",
+                   "(a)Sound your horn | (b)Speed up to clear the area quickly | (c)Slow down and pass with caution",
                    "(a)Speed up | (b)Reduce your speed | (c)Drive at the posted speed",
                    "(a)Across from your vehicle | (b)Who arrived before you | (c)On your left",
-                   "(a)During the first rain after a dry spell | (b)After it has been raining for awhile | (c)During a heavy downpour"]
+                   "(a)During the first rain after a dry spell | (b)After it has been raining for awhile | (c)Heavy downpour"]
 
 
 #Create the textbox UI
@@ -98,38 +104,38 @@ text_input = pygame_gui.elements.UITextEntryLine(relative_rect = pygame.Rect((80
 
 #If user got a correct answer, the a check mark will be shown with 0 transparency(255) and then become transparent(0)
 def correct_img(x, y):
+    
     check_mark.set_alpha(255)
-    #timer for 1 second(1000 millisecond)
+    #placed on screen
     window.blit(check_mark, (x, y))
-    #timer_sec = pygame.time.get_ticks() + 1000
-    #while pygame.time.get_ticks() < timer_sec:
-     #   window.blit(check_mark, (x, y))
+    #transparency on
     check_mark.set_alpha(0)
+    
     return
 
 
 #If user got a incorrect answer, the an 'x' mark will be shown with 0 transparency(255) and then become transparent(0)
 def incorrect_img(x, y): 
-    check_mark.set_alpha(255)   
-    #timer for 1 second(1000 millisecond)
+    #no transparency
+    check_mark.set_alpha(255)
+    #placed on screen
     window.blit(x_mark, (x, y))
-    #timer_sec = pygame.time.get_ticks() + 1000
-    #while pygame.time.get_ticks() < timer_sec:
-    #    window.blit(x_mark, (x, y))
+    #transparency on
     x_mark.set_alpha(0)
+
     return
 
     
 
 
-#This taxes in a question and what the user responded with and checks if their answer matches with 
-#the questions corresponding answer in the dictionary
+#This takes in a question and what the user responded with and checks if their answer matches with 
+#the questions' corresponding answer in the dictionary
 def check_correct(question, response):
     if(quest_ans[question].lower() == response.lower()):
         return True
    
 
-#Displays text that the user enters at the specified location and its font size
+#Displays text that the user enters at the specified location and with its font size
 def display_text(text_display, x, y, size):
     while True:
         #Creates a font with the font size parameter
@@ -145,126 +151,122 @@ def display_text(text_display, x, y, size):
         pygame.display.update()
 
 
-
+#Variable to store index of the question
 question_index = 0
+#The question side of the dictionary imported is converted to a list
 list_of_questions = list(quest_ans.keys())
-question_answered = False
-color_index = 0
-number_correct = 0
-number_incorrect = 0
+#lives/chances initialized
 lives = 3
+#variable that stores boolean value that will be used to control if the main loop will run or end
 run = True
-test_if_end = False
+#variable to store the index of the answers shown on the screen
 list_ans_index = 0
+#stores the question number(ex. "1. What is the date")
 question_number = 1
 
+#main loop 
 while run:
     
-    #background color
+    #background color is set 
     window.fill(premium_gray)
     #Turquoise colored heading. Parameters = pygame.draw.rect(screen, [red, blue, green], [left, top, width, height], filled)
     turq_rect = pygame.draw.rect(window, r_gray, (0,0, length, 75), 0)
     
     #Title of game created(1) then placed on screen(2)
-    text_title = font_largest.render("Mock Driving Test", True, black)
-    window.blit(text_title, (720, 28))
+    text_title = font1.render("Mock Driving Test", True, black)
+    window.blit(text_title, (700, 15))
 
-    text_exit = font_largest.render("Esc = Exit", True, black)
-    window.blit(text_exit, (1700, 28))
+    #Text created to inform the user how to exit the program(1), text placed on screen(2)
+    text_exit = font1.render("Esc = Exit", True, black)
+    window.blit(text_exit, (1600, 15))
 
-    #tip to user
-    text_tip = font3.render("*Enter the letter corresponding to the answer choice* Enter only the letters a, b, or c or else it will count as incorrect!", True, black)
+    #Text created to give a tip to the user(1), that text is placed on the screen(2)
+    text_tip = font2.render("*Enter the letter corresponding to the answer choice* Enter only the letters a, b, or c or else it will count as incorrect!", True, black)
     window.blit(text_tip,(100, 600))
     
 
 
 
-    #The refresh rate of the text box
-    refreshrate_ui = clock.tick(60)/750 #The /750 controls how fast the cursor blinks
+    #The refresh rate of the text box(the cursor blink)
+    refreshrate_ui = clock.tick(60)/750 #The /750 controls how fast the cursor blinks(it slows it down)
 
 
-    #Refresh rate for UI
-    #UI_RR = clock.tick(60)/1000
-
+    #The current question is stored in the variable and is decided by the question_index initialized outside the loop
     current_question = list_of_questions[question_index]
-    
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    #correct_score = font2.render("Number Correct: " + str(number_correct), True, black)
-    #incorrect_score = font2.render("Number Incorrect: " + str(number_incorrect), True, black)
-    #window.blit(correct_score, (0,0))
-    #window.blit(incorrect_score, (0, 35))
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    #Text to show chances is created(1), that text is displayed on the screen(2)
     lives_display = font1.render("Chances: " + str(lives), True, black)
-    window.blit(lives_display, (5, 28))
+    window.blit(lives_display, (5, 15))
 
+    #The text for the question shown on the screen is created
     question_on_screen = font_largest.render(str(question_number) + ". " + current_question, True, black)
-    answers_on_screen = font1.render(list_of_answers[list_ans_index], True, black)
+    #The text for the answer choices on the screen is created
+    answers_on_screen = font4.render(list_of_answers[list_ans_index], True, black)
+    #The answer is placed on the screen
     window.blit(answers_on_screen, (70, 300))
+    #The question is placed on the screen
     window.blit(question_on_screen, (70, 120))
-    clock.tick(60)
+    #The FPS variable is used to control the refresh rate of the loop
+    clock.tick(FPS)
     
-
+    #gets ever event that occurs(an event can be anything--button clicked--mouse clicked..etc)
     for event in pygame.event.get():
-        #Program ends when 'x' pressed
+        #Program ends when 'x' pressed or exit is clicked
         if event.type == pygame.QUIT:
             run = False
+        #if the user presses the escape key, the program will terminate
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    test_if_end = True
                     run = False
-       
+        #checks if the user has clicked the text box AND entered a value
         if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#text_box_entry":
+            #checks if their response is the correct answer to the question
             if(check_correct(current_question, event.text)):
+                #A checkmark is shown on the screen
                 correct_img(100, 100)
-                #Makes the textbox empty 
+                #Makes the textbox empty(so previous answer is not shown for next question) 
                 text_input.set_text("")
-                #increments the list number_correct by 1 
-                #!
-                number_correct += 1
-                #true if question has been answered
-                question_answered = True
-                question_number += 1
-            if(not(check_correct(current_question, event.text))):
-                incorrect_img(100, 100)
-                text_input.set_text("")
-
-                lives -= 1
-
-                #increments the list number_incorrect by 1 
-                #!
-                number_incorrect += 1
-
-                #true if question has been answered
-                question_answered = True
+                #The question number is incremented by 1(question 1, 2, 3, 4..)
                 question_number += 1
 
-            if(lives <= 0):
-                test_if_end = True
-                lives = 0
-                window.fill(blooming_red)
-
-                display_text("You Failed! You need more review.", 900, 300, 100)
-                
-                
-                   
-            if(question_index < len(list_of_questions) - 1):
-                question_index += 1
-                list_ans_index += 1
+            #if their answer is incorrect
             else:
+                #'x' mark is shown on the screen
+                incorrect_img(100, 100)
+                #Makes the textbox empty(so previous answer is not shown for next question)
+                text_input.set_text("")
+                #The lives(represents chances) is decremented by 1
+                lives -= 1
+                #The question number is incremented by 1(question 1, 2, 3, 4..)
+                question_number += 1
+            #checks if the users lives(chances) has reached 0
+            if(lives <= 0):
+                #The screen becomes red
+                window.fill(blooming_red)
+                #this text is displayed informing the user that they need to practice more
+                display_text("You Failed! You need more review.", 900, 300, 100)
+                run = False
+                
+            #If the variable question_index is less than the list of questions, then the game continues      
+            if(question_index < len(list_of_questions) - 1):
+                #index of the question is incremented
+                question_index += 1
+                #index of the answer is incremeneted
+                list_ans_index += 1
+            #If the previous statement is false, then the user has successfully answered all the questions without failing
+            else:
+                #The question on the screen becomes invisible
                 question_on_screen = font3.render("", True, black)
+                #invisible text is shown on screen
                 window.blit(question_on_screen, (130, 120))
-        
-
-                #!
-                #display_text("Final Score: " + str(number_correct) + "/" + str(len(list_of_questions)), 500, 300, 50)  
-                # ! 
-                test_if_end = True
+                #The background color becomes green
                 window.fill(victory_green)
+                #This text is displayed congratulating the user
                 display_text("Congratulations! You have passed your practice test", 900, 300, 100)
+                run = False
 
-
-            #event.text is what the user wrote and entered
-
+            
+        #This processes all the events that the text box gets
         manager.process_events(event)
         
     
@@ -272,9 +274,9 @@ while run:
         
     
 
-
+    #The textbox is updated at the refresh rate specified
     manager.update(refreshrate_ui)
-
+    #The textbox is repeatedly draw on the window 
     manager.draw_ui(window)
 
 
@@ -283,8 +285,8 @@ while run:
     
       
 
-        
+    #The display is updated every time the loop runs     
     pygame.display.update()
     
-
+#In the case that run = False in the loop, the program exits the loop and comes here, ending the program
 pygame.quit()
